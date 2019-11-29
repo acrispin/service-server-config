d3fa77b9ef812a5bb7c059bcef7f3b59c51588f9
Antonio Crispin <antonio.crispin@gmail.com>
2019-11-29 09:12:19 -0500

    first commit

---
+++ src/main/java/com/unicon/server/serviceserverconfig/ServiceServerConfigApplication.java
@@ -0,0 +1,13 @@
+package com.unicon.server.serviceserverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceServerConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceServerConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicon/server/serviceserverconfig/ServiceServerConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicon.server.serviceserverconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceServerConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
