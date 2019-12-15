15c86e4215ae949d552fdcbad13e901d9b7fba25
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-15 14:00:45 -0500

    rename package

--- src/main/java/com/unicon/server/api/config/ServiceServerConfigApplication.java
+++ src/main/java/com/unicon/server/api/config/ServiceServerConfigApplication.java
@@ -1,9 +1,13 @@
-package com.unicon.server.serviceserverconfig;
+package com.unicon.server.api.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * @author acrispin
+ * @since 12/15/2019
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ServiceServerConfigApplication {
--- src/test/java/com/unicon/server/api/config/ServiceServerConfigApplicationTests.java
+++ src/test/java/com/unicon/server/api/config/ServiceServerConfigApplicationTests.java
@@ -1,8 +1,12 @@
-package com.unicon.server.serviceserverconfig;
+package com.unicon.server.api.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author acrispin
+ * @since 12/15/2019
+ */
 @SpringBootTest
 class ServiceServerConfigApplicationTests {
 
